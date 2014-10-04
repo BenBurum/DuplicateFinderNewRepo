436f9e2cf2cf9075e5811499cf10481f38f5e8ff
BenBurum <benburum@gmail.com>
2014-10-04 05:23:41 -0400

    Maven Commit
    Forgot to import and compile maven project before.  Hopefully it will
    work this time.

--- burum-brown/src/main/java/com/agile/findduplicates/FinderPanel.java
+++ burum-brown/src/main/java/com/agile/findduplicates/FinderPanel.java
@@ -7,21 +7,7 @@ import javax.swing.*;
  */
 public class FinderPanel {
     private JPanel panel1;
-    private JTextField dirPathTextField;
-    private JButton findDirectoryButton;
+    private JTree tree1;
     private JTextArea textArea1;
     private JButton analyzeButton;
-    private JTree duplicateFileTree;
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("FinderPanel");
-        frame.setContentPane(new FinderPanel().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
 }
